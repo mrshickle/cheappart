144f122fe46886f66adcd372472e4156d388f653
davidmartirosyan <david.mart@sadasystems.com>
2016-03-16 22:55:11 -0700

    initial commit

---
+++ Gulpfile.js
@@ -0,0 +1,96 @@
+var gulp = require('gulp'),
+    //browserSync = require('browser-sync'),
+    sass = require('gulp-sass'),
+    inject = require('gulp-inject'),
+    wiredep = require('wiredep').stream,
+    browserSync = require('browser-sync').create();
+
+
+
+gulp.task('sass', function () {
+  gulp.src('./css/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+//gulp.task('sass:watch', function () {
+//  gulp.watch('./css/sass/**/*.scss', ['sass']);
+//});
+
+
+gulp.task('inject', function() {
+  var sources = gulp.src(['./js/**/*.js', './css/**/*.css'])
+  return gulp.src('index.html')
+      .pipe(inject(sources, {
+        read: false
+      }))
+      .pipe(gulp.dest('./'))
+})
+
+gulp.task('wiredep', function() {
+  return gulp.src('index.html')
+      .pipe(wiredep({
+        directory: './bower_components'
+      }))
+      .pipe(gulp.dest('./'))
+})
+
+
+gulp.task('watch', function() {
+
+    gulp.watch('./css/sass/**/*.scss', ['sass']);
+    gulp.watch('./css/**/*.css', ['inject']);
+    gulp.watch('./js/**/*.js', ['inject']);
+    gulp.watch('./bower.json', ['wiredep']);
+
+    gulp.watch(['./**/*.*']).on('change', browserSync.reload);
+
+
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        notify:false,
+        proxy: "localhost:8080",
+        port:8081
+        // server: {
+        //     baseDir: "./"
+        // }
+    });
+});
+
+
+gulp.task('browser-sync-tablet', function() {
+    browserSync.init({
+        notify:false,
+        proxy: "localhost:8080",
+        port:8089
+        // server: {
+        //     baseDir: "tablet",
+        //     routes: {
+        //         "/bower_components": "bower_components",
+        //         "/css": "css",
+        //         "/img": "img",
+        //         "/js": "js",
+        //         "/views": "views",
+        //         "/data": "data"
+        //     }
+        // }
+    });
+});
+
+gulp.task('serve', function() {
+
+    //browserSync.init({
+    //    proxy: "localhost:8080",
+    //    notify:false
+    //});
+    //
+    //gulp.watch("./css/**/*.css").on('change', browserSync.reload);
+    //gulp.watch("./**/*.html").on('change', browserSync.reload);
+    //gulp.watch("./js/**/*.js").on('change', browserSync.reload);
+
+});
+
+gulp.task('default', ['watch', 'sass', 'browser-sync']);
+gulp.task('tablet', ['watch', 'sass', 'browser-sync-tablet']);
